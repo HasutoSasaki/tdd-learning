212ff7afefad74869e518b0106bec3daf016c93e
HasutoSasaki <hasuto.sa@gmail.com>
2025-09-26 23:36:05 +0900

    feat: 三角測量の実装コード追加. - 少し違った角度から問題について考えてテストを実装する.

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -1,7 +1,9 @@
 package money;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
     @Test
@@ -11,5 +13,10 @@ public class MoneyTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
